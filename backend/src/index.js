6712ddee6fbc81d9259a3a506b52f9902ed42d5f
William Torres <contatowilliamtorres@gmail.com>
2021-03-05 11:50:58 -0300

    Aplicaçao funcional

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,53 +1,66 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+const projects = [];
 
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
+  const { title } = request.query;
 
-  console.log({title, owner})
+  const results = title 
+    ? projects.filter(project => project.title.includes(title))
+    : projects
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2'
-  ]);
+  return response.json(results);
 })
 
 app.post('/projects', (request, response) => {
-  const body = request.body
+  const {title, owner} = request.body
 
-  console.log(body)
+  const project = {id: uuidv4(), title, owner}
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3'
-  ]);
+  projects.push(project)
+
+  return response.json(project);
 })
 
 app.put('/projects/:id', (request, response) => {
-  const params = request.params
+  const { id } = request.params
+  const {title, owner} = request.body
 
-  console.log(params)
+  const projectIndex = projects.findIndex(project => project.id === id)
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3'
-  ]);
+  if(projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found'})
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+  projects[projectIndex] = project
+
+  return response.json(project);
 })
 
 app.delete('/projects/:id', (request, response) => {
-  const params = request.params
+  const { id } = request.params
 
-  console.log(params)
+  const projectIndex = projects.findIndex(project => project.id === id)
 
-  return response.json([
-    'Projeto 2',
-    'Projeto 3'
-  ]);
+  if(projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found'})
+  }
+
+  // splice = método para remover informação do array
+  // splice(índice a ser removido, quantas posições remover a partir deste índice)
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send();
 })
 
 app.listen(3333, () => {
