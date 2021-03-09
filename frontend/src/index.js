f300db81bc9990c30263469223a3d481500e27e0
William Torres <contatowilliamtorres@gmail.com>
2021-03-09 18:21:57 -0300

    Componentizaçao

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,5 @@
-import {soma} from './soma'
-console.log(soma(1,9))
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+
+render(<App />, document.getElementById('app'))
