c10091226dac779c444174031de3b43a63dfb1cb
William Torres <contatowilliamtorres@gmail.com>
2021-03-12 17:46:24 -0300

    Quando adicionar tipos

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,9 +1,7 @@
 import express from 'express'
-
+import { helloWorld } from './routes'
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World with TypeScript'})
-})
+app.get('/', helloWorld)
 
 app.listen(3333)
