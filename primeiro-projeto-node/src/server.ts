b2347e68755ecde57ef114e786ab9e6030e2739f
William Torres <contatowilliamtorres@gmail.com>
2021-03-13 14:14:55 -0300

    Padroes de projeto com Eslint, Prettier e EditorC.

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack' })
-})
+  return response.json({ message: 'Hello GoStack' });
+});
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
