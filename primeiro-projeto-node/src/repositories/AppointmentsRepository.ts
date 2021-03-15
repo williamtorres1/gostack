65c01e5c86ecbdb52dfca10b71517d31a0a7ce5d
William Torres <contatowilliamtorres@gmail.com>
2021-03-15 20:08:37 -0300

    Listando agendamentos

--- primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -16,6 +16,10 @@ class AppointmentsRepository {
     return appointment;
   }
 
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment =>
       isEqual(date, appointment.date),
