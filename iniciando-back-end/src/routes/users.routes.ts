00e7ac033f18f48b8c75b2657f80857f063f634d
William Torres <contatowilliamtorres@gmail.com>
2021-04-12 14:52:54 -0300

    Criptografia de senha

--- iniciando-back-end/src/routes/users.routes.ts
+++ iniciando-back-end/src/routes/users.routes.ts
@@ -11,7 +11,15 @@ usersRouter.post('/', async (request, response) => {
 
     const user = await createUser.execute({ name, email, password });
 
-    return response.status(201).json(user);
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.status(201).json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
