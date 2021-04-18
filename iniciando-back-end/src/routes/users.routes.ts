ffda757795519e7f26bc69bbf45b2a86f8829fa5
William Torres <contatowilliamtorres@gmail.com>
2021-04-18 10:55:17 -0300

    Upload de arquivos

--- iniciando-back-end/src/routes/users.routes.ts
+++ iniciando-back-end/src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/Upload';
+
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -24,5 +29,18 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      return response.json(request.file);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
