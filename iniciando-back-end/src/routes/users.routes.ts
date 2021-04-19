3b8e1800299b2633233cda2564a49b075b5e5272
William Torres <contatowilliamtorres@gmail.com>
2021-04-19 15:57:48 -0300

    Lidando com erros

--- iniciando-back-end/src/routes/users.routes.ts
+++ iniciando-back-end/src/routes/users.routes.ts
@@ -10,13 +10,34 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+  const user = await createUser.execute({ name, email, password });
 
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    avatar: user.avatar,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
+
+  return response.status(201).json(userWithoutPassword);
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
     const userWithoutPassword = {
       id: user.id,
       name: user.name,
@@ -26,36 +47,7 @@ usersRouter.post('/', async (request, response) => {
       updated_at: user.updated_at,
     };
 
-    return response.status(201).json(userWithoutPassword);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
-
-usersRouter.patch(
-  '/avatar',
-  ensureAuthenticated,
-  upload.single('avatar'),
-  async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
-      const userWithoutPassword = {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        avatar: user.avatar,
-        created_at: user.created_at,
-        updated_at: user.updated_at,
-      };
-
-      return response.json(userWithoutPassword);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(userWithoutPassword);
   },
 );
 
