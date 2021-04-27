1dba7d63213cd2db81765e1d1834690f673963d0
William Torres <contatowilliamtorres@gmail.com>
2021-04-27 11:05:18 -0300

    Criando Rotas

---
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => (
+  <div>
+    <h1>Hello GoStack from Dashboard</h1>
+  </div>
+);
+
+export default Dashboard;
---
+++ primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Repository: React.FC = () => (
+  <div>
+    <h1>Hello GoStack from Repository</h1>
+  </div>
+);
+
+export default Repository;
